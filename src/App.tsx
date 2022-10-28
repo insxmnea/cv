ce42e33b4fa8ec59decf59878685e82574dd6665
klltx <kostya.svetashov.01@gmail.com>
2022-10-28 22:02:24 +0400

    add packages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       
       <nav>
         <div>
-        <a href="#">INFO</a>
+        <a href="#" className='text-3xl text-gray-700 font-bold mb-5'>INFO</a>
         </div>
       </nav>
       <main className="container">
