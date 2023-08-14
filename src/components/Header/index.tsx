ddb47aa2d35dc16317e27c816872eca7de46cdfe
klltx <kostya.svetashov.01@gmail.com>
2023-08-14 17:56:57 +0400

    fix: images url

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header className='flex py-8 justify-between'>
-      <div className='text-white'>klltx</div>
+      <div className='text-white'>1317n0d3</div>
       <div className='flex'>
         <a className='current'>home</a>
         {/* <a className='link'>works</a>
